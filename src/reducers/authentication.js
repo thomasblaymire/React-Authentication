3d5dc4c703ecebcdab85e8a043066485b287c41f
Tom Blaymire <tblaymire1994@gmail.com>
2017-12-17 22:01:43 +0000

    First commit

---
+++ src/reducers/authentication.js
@@ -0,0 +1,16 @@
+import {
+  CHANGE_AUTH
+} from '../actions/types';
+
+export default function(state = false, action) {
+  switch(action.type) {
+    case CHANGE_AUTH:
+      return action.payload;
+  }
+
+  return state;
+}
+
+
+//State is default as the user is not defauled to be logged in
+  //Boolean: If the user is logged in log them in otherwise now
